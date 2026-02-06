e107e6f5fd40d97c13bf24da03a44a056537dc4b
Lazar Nenovski <lazar.nenovski@gmail.com>
2026-02-06 12:19:59 +0000

    More defined header and footer, added preliminary navigation

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,24 @@
+"use client";
+import Link from "next/link"
+import { usePathname } from 'next/navigation';
 
 function Footer() {
+	const currentRoute = usePathname();
+
 	return <>
-			<div className="footer ">
+			<div className="footer">
 				<footer>
-					<h1>Footer</h1>
+					<nav>
+						<Link className={currentRoute === "/" 
+							? "NavButton-active" 
+							: "NavButton-nonactive"} href={"/"}>Home</Link>
+						<Link className={currentRoute === "/Wardrobe" 
+							? "NavButton-active" 
+							: "NavButton-nonactive"} href={"/Wardrobe"}>Wardrobe</Link>
+						<Link className={currentRoute === "/Add" 
+							? "NavButton-active" 
+							: "NavButton-nonactive"} href={"/Add"}>Add</Link>
+					</nav>
 				</footer>
 			</div>
 		</>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,19 @@ import Link from "next/link";
 
 function Header() {
     return (
-        <div className="header ">
+        <div className="header">
             <header>
+                <div className="AppTitle">
+                    <h1>
+                        <b>Wardrobe Forecast</b>
+                    </h1>
+                </div>
+                <div className="Profile">
                 <nav>
                     <Link href={"/"}>Home</Link>
                     <Link href={"/Hello"}>Hello</Link>
                 </nav>
+                </div>
             </header>
         </div>
     );
