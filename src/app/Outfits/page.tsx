ce84c844500b7bdfedadbcc55c4df86eb7b55f38
Lazar Nenovski <lazar.nenovski@gmail.com>
2026-02-06 18:48:47 +0000

    Vercel hand-import module

--- src/app/Outfits/page.tsx
+++ src/app/Outfits/page.tsx
@@ -2,8 +2,16 @@
 
 import { useState, useEffect } from 'react';
 
-import {  OutfitData } from '../types/form'; /* For Vercel */
+//import {  OutfitData } from '../types/form'; /* For Vercel */
 import styles from './page.module.css';
+
+// For Vercel
+interface OutfitData {
+  date: string;
+  composition: string;
+  cost: number;
+}
+
 
 function Wardrobe() {
 	const [submissions, setSubmissions] = useState<OutfitData[]>([]);
